e7a56e2ecb1dc164f7935cdec1fbefb0b3b7cc58
Fabian Ramirez <fabianramirez0072@gmail.com>
2023-12-22 15:42:39 -0500

    Login Exitoso

--- Gym/Site/Login.Designer.cs
+++ Gym/Site/Login.Designer.cs
@@ -28,19 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            txtUser = new TextBox();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(150, 361);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Iniciar Session";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(150, 154);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(100, 23);
+            txtUser.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(150, 234);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(100, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(150, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Usuario";
+            label1.Click += Login_Load;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(150, 207);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Contraseña ";
             // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(469, 502);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUser);
+            Controls.Add(button1);
             Name = "Login";
             Text = "Form1";
             Load += Login_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox txtUser;
+        private TextBox txtPassword;
+        private Label label1;
+        private Label label2;
     }
 }
--- Gym/Site/Login.cs
+++ Gym/Site/Login.cs
@@ -4,16 +4,54 @@ namespace Gym
 {
     public partial class Login : Form
     {
-        private readonly CredentialService service;
+        private readonly CredentialService _service;
         public Login()
         {
-            service = new CredentialService();
+            _service = new CredentialService();
             InitializeComponent();
         }
 
         private void Login_Load(object sender, EventArgs e)
         {
-            var s = service.ValidateUsuario("admin");
+
         }
+
+        private bool validade()
+        {
+            if (txtUser.Text.Equals("") ||  txtPassword.Text.Equals(""))
+            {
+                MessageBox.Show("Verifique los campos", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (validade())
+            {
+                var respuesta = _service.Login(txtUser.Text, txtPassword.Text);
+                if (respuesta.Success) {
+                    MessageBox.Show(respuesta.Message, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+                    MessageBox.Show(respuesta.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LimpiarCampos(Control parent)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                if (c is TextBox) c.Text = "";
+                if (c.Controls.Count > 0) LimpiarCampos(c);
+            }
+            txtUser.Focus();
+        }
+
     }
 }
